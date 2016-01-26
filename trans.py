c52f9b192a04bfbbfd51321cfb86757c96656bf4
James McGregor <jay911144@gmail.com>
2016-01-26 07:49:02 -0800

    Transaction scraper

--- trans.py
+++ trans.py
@@ -9,8 +9,13 @@ from googlefinance import getQuotes
 url = "http://www.marketwatch.com/game/stock-fears/portfolio/transactionhistory?name=Dustin%20Lien&p=1354179"
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html.parser")
+tickers = []
 
-tickers = soup.find_all("td")
+for item in soup.find_all("td"):
+    if item.find(class_='pubDate'):
+	break
+    tickers.append(item)
 
 for item in tickers:
-    print item.text.replace("\t", "").replace("\r", "").replace("\n", "")
+    if item.parent.name == "tr":
+        print item.text.replace("\t", "").replace("\r", "").replace("\n", "")
