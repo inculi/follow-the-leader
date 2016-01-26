a1653025788447b503876714e82e230206643d2e
James McGregor <jay911144@gmail.com>
2016-01-26 11:45:06 -0800

    Get transaction history; added multiple page support

--- trans.py
+++ trans.py
@@ -6,16 +6,47 @@ from moira import moira
 from bs4 import BeautifulSoup
 from googlefinance import getQuotes
 
+#Output
+symbols = []
+orderdate = []
+transdate = []
+ordertype = []
+orderamount = []
+orderprice = []
+
+
+
+def getURLs(url):
+    page = 0
+    index = url.find('name=')
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    people = soup.find_all("p")
+    for item in people:
+        if item.parent.name == 'nav':
+            re1='.*?'	# Non-greedy match on filler
+            re2='\\d+'	# Uninteresting: int
+            re3='.*?'	# Non-greedy match on filler
+            re4='\\d+'	# Uninteresting: int
+            re5='.*?'	# Non-greedy match on filler
+            re6='(\\d+)'	# Integer Number 1
+
+            rg = re.compile(re1+re2+re3+re4+re5+re6,re.IGNORECASE|re.DOTALL)
+            m = rg.search(item.text)
+            if m:
+                int1=m.group(1)
+                total = int(int1)
+            for page in xrange (0,total,10):
+                getHistory(url[:index] + 'index=' + str(page) + '&' + url[index:])
+
+
+
 def getHistory(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
     tickers = []
-    symbols = []
-    orderdate = []
-    transdate = []
-    ordertype = []
-    orderamount = []
-    orderprice = []
 
     x = 0
 
@@ -70,6 +101,6 @@ def getHistory(url):
     print "Order price"
     for item in orderprice:
         print item
-#End of def
 
-getHistory("http://www.marketwatch.com/game/stock-fears/portfolio/transactionhistory?name=Dustin%20Lien&p=1354179")
+
+getURLs("http://www.marketwatch.com/game/stock-fears/portfolio/transactionhistory?name=Dustin%20Lien&p=1354179")
