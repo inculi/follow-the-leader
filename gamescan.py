240ae531dc135f7ac2df5f2ed18241f7bf386c25
Manwholikespie <rdominguez@me.com>
2016-01-27 15:54:32 -0600

    Update gamescan.py, Create userscan.py
    Added some more things to gamescan. Made it so that it only gathers 1/3 of the users in a game (rounded of course). This is done because usually only the top third of users are even positive. As for userscan, this is the beginning of the program that shall be called when the first round of user data has been gathered (done to save time). It shall analyze the performance tab to see how well they have done for the past 10 days, and shall begin constructing the user ranking based off of this information.

--- gamescan.py
+++ gamescan.py
@@ -2,9 +2,10 @@ import re
 import json
 import time
 import moira
+import random
 import requests
-import pandas as pd
 import numpy as np
+import pandas as pd
 from moira import moira
 from bs4 import BeautifulSoup
 #from googlefinance import getQuotes
@@ -14,16 +15,6 @@ from bs4 import BeautifulSoup
     Using BeautifulSoup, this will scrape a game's user data. Using this data,
     we can rank player performance, and find the trades that they have used.
 """
-
-# constants that we will need later on in the program.
-ranks = []
-names = []
-returns = []
-player_urls = []
-transaction_urls = []
-networths = []
-total_cash_returns = []
-todays_percent_returns = []
 
 def pageCounter(gameName):
     """
@@ -64,7 +55,7 @@ def pageCounter(gameName):
                 print("There are " + str(total) + " players in the game.")
 
     # find the URLs of all the pages of a game
-    for x in xrange (0,total,10):
+    for x in xrange (0,int(round(((1.0/3.0)*total))),10):
         # http://www.marketwatch.com/game/stock-fears/ranking?index=10&total=974
         if x < 10: # the first page's url can do with or without the index pieces
             url = ("http://www.marketwatch.com/game/" +
@@ -84,6 +75,18 @@ def pageCounter(gameName):
 
     return pages
 
+# constants that we will need later on in the program.
+ranks = []
+names = []
+returns = []
+player_urls = []
+transaction_urls = []
+performance_urls = []
+networths = []
+total_cash_returns = []
+todays_percent_returns = []
+amtOfTrades = []
+
 def pageScan(pageurl):
     # url = "http://www.marketwatch.com/game/bijans/ranking"
     # setting up BeautifulSoup
@@ -96,79 +99,175 @@ def pageScan(pageurl):
 
     td_names = soup.find_all("td", {"class" : "name"})
 
-    # the returns can be found in the fourth element of the <tr> tag.
-    td_returns = soup.find_all("td", {"class" : "numeric positive"})
-
     player_url = soup.findAll('a', href=re.compile('/portfolio/holdings\?name='))
 
     td_networth = soup.find_all("em")
-
-    # Filter for data
-    for item in player_url:
-        #print(item.get('href'))
-        url = 'http://marketwatch.com' + str(item.get('href'))
-        player_urls.append(url)
-        transaction_urls.append(url.replace("holdings", "transactionhistory"))
-
-    for item in td_rank:
-        good_data = item.text.replace("\t", "").replace("\r", "").replace("\n", "")
-        #print(good_data)
-        ranks.append(good_data)
-
-    for item in td_names:
-        good_data = item.text.replace("\t", "").replace("\r", "").replace("\n", "")
-        #print(good_data)
-        names.append(good_data)
 
     # total cash returns
     for row in soup.findAll('table')[0].tbody.findAll('tr'):
         cash_return_column = row.findAll('td')[4:5]
         for item in cash_return_column:
-            total_cash_returns.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace("$", "").replace(",","")))
+            if 'B' not in item.text:
+                total_cash_returns.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace("$", "").replace(",","")))
+            if 'B' in item.text:
+                total_cash_returns.append(float(0))
 
     # today's percent returns
     for row in soup.findAll('table')[0].tbody.findAll('tr'):
         percent_return_column = row.findAll('td')[3:4]
         for item in percent_return_column:
-            todays_percent_returns.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace("%", "").replace(",","")))
+            if 'B' not in item.text:
+                todays_percent_returns.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace("%", "").replace(",","")))
+            if 'B' in item.text:
+                todays_percent_returns.append(float(0))
+
+    # Filter for data
+    for item in td_rank:
+        ranks.append(item.text.replace("\t", "").replace("\r", "").replace("\n", ""))
+
+    for item in td_names:
+        names.append(item.text.replace("\t", "").replace("\r", "").replace("\n", ""))
 
     for item in td_networth:
-        networths.append(item.text)
+        networths.append(float(item.text.replace("$","").replace(",","").replace(")","").replace("(","-")))
 
+    for item in player_url:
+        url = 'http://marketwatch.com' + str(item.get('href'))
+        player_urls.append(url)
+        transaction_urls.append(url.replace("holdings", "transactionhistory"))
+        performance_urls.append(url.replace("holdings", "performance"))
+
+    for row in soup.findAll('table')[0].tbody.findAll('tr'):
+        trades_column = row.findAll('td')[5:6]
+        for item in trades_column:
+            amtOfTrades.append(float(item.text))
 
 def gameScan(gameName):
     pageUrls = pageCounter(gameName)
-
+    index = 0
     for url in pageUrls:
+        index+=1
         pageScan(url)
         # time.sleep(1)
-        print("Finished " + str(url))
+        print("Finished " + str(index) + " of " + str(len(pageUrls)))
+
+
+
+games = [] # list that the game names will be stored in (for processing by gameScan)
+
+def getGames(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    index = url.find('sort=')
+    for item in soup.find_all("td", {'class' : 'name'}):
+        #print item
+        out = str(item).rsplit("href=\"/game/", 1)[1]
+        games.append(out.rsplit("\" title", 1)[0])
+
+def getGameURLs(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    index = url.find('sort=')
+
+    people = soup.find_all("p")
+    for item in people:
+        if item.parent.name == 'nav':
+            re1='.*?'	# Non-greedy match on filler
+            re2='\\d+'	# Uninteresting: int
+            re3='.*?'	# Non-greedy match on filler
+            re4='\\d+'	# Uninteresting: int
+            re5='.*?'	# Non-greedy match on filler
+            re6='(\\d+)'	# Integer Number 1
+
+            rg = re.compile(re1+re2+re3+re4+re5+re6,re.IGNORECASE|re.DOTALL)
+            m = rg.search(item.text)
+            if m:
+                int1=m.group(1)
+                total = int(int1)
+            for page in xrange (0, 100, 10):
+                getGames(url[:index] + 'index=' + str(page) + '&' + url[index:])
+
+    for item in games:
+        print item
 
 
 ### MAIN FUNCTION
-gameScan("hockinson-4")
+getGameURLs("http://www.marketwatch.com/game/find?sort=NumberOfPlayers&descending=True")
+
+gameCountIndex = 0
+for game in games:
+    gameCountIndex += 1
+    print("Scanning " + str(game) + ": (" + str(gameCountIndex) + "/" + str(len(games)) + ")")
+    gameScan(game)
+del gameCountIndex
 
 # print our findings
 print("\n\nRanks:")
 print(len(ranks))
 print("\n\nNames:")
 print(len(names))
-print("\n\nCash Returns ($):")
+print("\n\nCash Returns:")
 print(len(total_cash_returns))
-print("\n\nPercent Returns (%):")
+print("\n\nPercent Returns:")
 print(len(todays_percent_returns))
 print("\n\nPlayer URLs:")
 print(len(player_urls))
-print("\n\nTransaction URLs:")
+print("\n\Performance URLs:")
+print(len(performance_urls))
 
 d = {
     'Name' : pd.Series(names, index=ranks),
     'Net Worth' : pd.Series(networths, index=ranks),
     'Today\'s Returns' : pd.Series(todays_percent_returns, index=ranks),
     'Total Cash Returns' : pd.Series(total_cash_returns, index=ranks),
+    'Trades' : pd.Series(amtOfTrades, index=ranks),
     'Player URL' : pd.Series(player_urls, index=ranks),
-    'Transaction URL' : pd.Series(transaction_urls, index=ranks)
+    'Transaction URL' : pd.Series(transaction_urls, index=ranks),
+    'Performance URL' : pd.Series(performance_urls, index=ranks)
     }
 
 df = pd.DataFrame(d)
 print df
+df.to_csv("dataframe2.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
---
+++ userscan.py
@@ -0,0 +1,107 @@
+import re
+import json
+import time
+import moira
+import requests
+import pandas as pd
+import numpy as np
+from moira import moira
+from bs4 import BeautifulSoup
+
+"""
+    Once all the data for the top 100 games has been indexed, this shall go
+    through those users to append more relevant information to their data
+    profiles so that they can be ranked more accurately.
+"""
+
+
+
+performance_Dates = []
+performance_Ranks = []
+performance_Cash = []
+performance_Cash_Int = []
+performance_Margin_Cost = []
+performance_Total_Equity = []
+performance_Returns = []
+
+def getPerformance(inputURL):
+    url = inputURL
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+
+
+    # [6:7] Return
+    # [5:6] total equity
+    # [4:5] margin cost
+    # [3:4] cash Int
+    # [2:3] cash
+    # [1:2] rank
+    # [0:1] date
+
+    #date
+    for row in soup.findAll('table')[0].tbody.findAll('tr'):
+        data_column = row.findAll('td')[0:1]
+        for item in data_column:
+            performance_Dates.append(item.text.replace("\t", "").replace("\r", "").replace("\n", ""))
+
+    # rank
+    for row in soup.findAll('table')[0].tbody.findAll('tr'):
+        data_column = row.findAll('td')[1:2]
+        for item in data_column:
+            performance_Ranks.append(item.text.replace("\t", "").replace("\r", "").replace("\n", ""))
+
+    # cash
+    for row in soup.findAll('table')[0].tbody.findAll('tr'):
+        data_column = row.findAll('td')[2:3]
+        for item in data_column:
+            performance_Cash.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace("$","").replace(",","")))
+
+    # cash int
+    for row in soup.findAll('table')[0].tbody.findAll('tr'):
+        data_column = row.findAll('td')[3:4]
+        for item in data_column:
+            performance_Cash_Int.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace("$","").replace(",","")))
+
+    # margin cost
+    for row in soup.findAll('table')[0].tbody.findAll('tr'):
+        data_column = row.findAll('td')[4:5]
+        for item in data_column:
+            performance_Margin_Cost.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace("$","").replace(",","")))
+
+    # total equity
+    for row in soup.findAll('table')[0].tbody.findAll('tr'):
+        data_column = row.findAll('td')[5:6]
+        for item in data_column:
+            performance_Total_Equity.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace("$","").replace(",","")))
+
+    # returns
+    for row in soup.findAll('table')[0].tbody.findAll('tr'):
+        data_column = row.findAll('td')[6:7]
+        for item in data_column:
+            performance_Returns.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace("%","").replace(",","")))
+
+
+getPerformance("http://www.marketwatch.com/game/stock-fears/portfolio/performance?name=Dustin%20Lien&p=1354179")
+
+for element in performance_Dates:
+    print element
+
+for element in performance_Ranks:
+    print element
+
+for element in performance_Cash:
+    print element
+
+for element in performance_Cash_Int:
+    print element
+
+for element in performance_Margin_Cost:
+    print element
+
+for element in performance_Total_Equity:
+    print element
+
+for element in performance_Returns:
+    print element
+#
