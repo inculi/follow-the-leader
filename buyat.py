4c6805bb5b4cdef0ed76d4957cc4b42ede3a9656
James McGregor <James McGregor>
2016-02-02 08:40:53 -0600

    Changed price

--- buyat.py
+++ buyat.py
@@ -12,12 +12,11 @@ import login as l
 token = l.login()
 game = 'meisenheimer'
 
-
 while True:
     #time.sleep(1)
     astring = moira.stock_search(token, game, 'TSLA')
     print astring['price']
-    if float(astring['price']) >= 210:
+    if float(astring['price']) >= 205:
         print 'yes, selling 100 shares'
         moira.order(token, 'meisenheimer', 'Sell', 'STOCK-XNAS-TSLA', 100)
     else:
