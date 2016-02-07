2982eafef0f4708aa006248cc0668b5ee85d16b2
Manwholikespie <rdominguez@me.com>
2016-02-06 23:28:24 -0600

    Update mmutils.py
    Forgot to up the actual algorithm and stuff for this...

--- mmutils.py
+++ mmutils.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import mmutils as mm
 
 """
 
@@ -42,6 +43,35 @@ def appendData(startColumn, finishColumn, listName, dataType, inputUrl):
             data_column = row.findAll('td')[startColumn:finishColumn]
             for item in data_column:
                 listName.append(float(item.text.replace("\t", "").replace("\r", "").replace("\n", "").replace(",","")))
+
+def getConsecutiveDays(url):
+    # set up constants
+    positives = 0
+    negatives = 0
+    zeroes = 0
+
+    # set up bs4
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    dayReturns = []
+
+    # manipulate data
+    mm.appendData(6, 7, dayReturns, 'percent', url)
+    # for x in xrange(0,int(len(dayReturns)-1)):
+    #     print (dayReturns[x] - dayReturns[x+1])
+    #
+    # print("==========")
+    for x in xrange(0,int(len(dayReturns)-1)):
+        if (dayReturns[x] - dayReturns[x+1]) < 0:
+            negatives+=1
+        if (dayReturns[x] - dayReturns[x+1]) > 0:
+            positives+=1
+        elif (dayReturns[x] - dayReturns[x+1]) == 0:
+            zeroes+=1
+    numerator = float(len(dayReturns) + positives - negatives)
+    denominator = float((2 * len(dayReturns)) + zeroes)
+    return (numerator / denominator) * 100
+
 def printAll(variable):
     for item in variable:
         print item
